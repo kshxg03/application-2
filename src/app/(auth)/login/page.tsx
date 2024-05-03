85b851d3147b27cf1bf0f2639cec8cbcac86e669
Nishan Raut <nishanraut@kushfintech.com>
2024-05-03 23:04:51 +0545

    Login Update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,53 @@
-import React from "react";
+"use client";
+import { CustomError } from "@/app/common/errors/custom.error";
+import { LoginCommon } from "@/app/common/helper/login.request";
 import {
-  Container,
-  Typography,
-  TextField,
-  Button,
+  ReturnProps,
+  validationForm,
+} from "@/app/common/helper/login.validation";
+import {
   Box,
+  Button,
+  Container,
   Paper,
+  TextField,
+  Typography,
 } from "@mui/material";
 import Link from "next/link";
+import { useState } from "react";
 
 const LoginPage = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [userNameEmptyError, setUserNameEmptyError] = useState("");
+  const [passwordEmptyError, setPasswordEmptyError] = useState("");
+
+  async function HandleLogin() {
+    try {
+      const validateForm: ReturnProps = validationForm(userName, password);
+      if (validateForm.isEmpty) {
+        if (validateForm.forUserName) {
+          setUserNameEmptyError(validateForm.forUserName);
+        }
+        if (validateForm.forPassword) {
+          setPasswordEmptyError(validateForm.forPassword);
+        }
+      } else {
+        const response = await LoginCommon({ userName, password });
+        console.log("This is Response: ", response.Data);
+      }
+    } catch (error) {
+      if (error instanceof CustomError) {
+        console.log("This is Error in fetch: ", error._error);
+        if (error._error.Message instanceof Array) {
+          //This is not require since every thing is handle by frontend
+        }
+        setErrorMessage(error._error.Message);
+        console.log("This is Error: ", error._error.Message);
+      }
+    }
+  }
   return (
     <Container
       component="main"
@@ -66,18 +104,37 @@ const LoginPage = () => {
           <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
             Login
           </Typography>
-          <TextField margin="normal" required fullWidth label="Username" />
+          {errorMessage}
           <TextField
             margin="normal"
             required
             fullWidth
-            label="Password"
+            placeholder={userNameEmptyError || "UserName"}
+            onChange={(e) => {
+              setUserName(e.target.value);
+              if (e.target.value.trim() !== "") {
+                setUserNameEmptyError("");
+              }
+            }}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            placeholder={passwordEmptyError || "Password"}
             type="password"
+            onChange={(e) => {
+              setPassword(e.target.value);
+              if (e.target.value.trim() !== "") {
+                setPasswordEmptyError("");
+              }
+            }}
           />
           <Button
             type="submit"
             fullWidth
             variant="contained"
+            onClick={HandleLogin}
             sx={{
               mt: 3,
               backgroundColor: "black",
